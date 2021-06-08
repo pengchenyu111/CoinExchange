146375dfa5c14812822bce4749eef0078107904f
pengchenyu <1106998960@qq.com>
2021-06-08 16:36:56 +0800

    初步搭建网关服务

---
+++ coin-exchange/coin-iaas/gateway-server/src/main/java/com/bjsxt/GatewayServerApplication.java
@@ -0,0 +1,18 @@
+package com.bjsxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author PengChenyu
+ * @since 2021-06-08 16:12:59
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+}
