446d4f2c6fc81f4b9a89b36020263b19ca59bfb9
pengchenyu <1106998960@qq.com>
2021-06-11 17:35:56 +0800

    员工管理的CRUD

--- coin-exchange/coin-admin/admin-service/src/main/java/com/bjsxt/domain/SysUser.java
+++ coin-exchange/coin-admin/admin-service/src/main/java/com/bjsxt/domain/SysUser.java
@@ -1,25 +1,24 @@
 package com.bjsxt.domain;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+
 import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * 
  * @author PengChenyu
  * @since 2021-06-10 16:59:16
  */
+
 /**
-    * 平台用户
-    */
-@ApiModel(value="com-bjsxt-domain-SysUser")
+ * 平台用户
+ */
+@ApiModel(value = "com-bjsxt-domain-SysUser")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -29,76 +28,83 @@ public class SysUser {
      * 主键
      */
     @TableId(value = "id", type = IdType.INPUT)
-    @ApiModelProperty(value="主键")
+    @ApiModelProperty(value = "主键")
     private Long id;
 
     /**
      * 账号
      */
     @TableField(value = "username")
-    @ApiModelProperty(value="账号")
+    @ApiModelProperty(value = "账号")
     private String username;
 
     /**
      * 密码
      */
     @TableField(value = "`password`")
-    @ApiModelProperty(value="密码")
+    @ApiModelProperty(value = "密码")
     private String password;
 
     /**
      * 姓名
      */
     @TableField(value = "fullname")
-    @ApiModelProperty(value="姓名")
+    @ApiModelProperty(value = "姓名")
     private String fullname;
 
     /**
      * 手机号
      */
     @TableField(value = "mobile")
-    @ApiModelProperty(value="手机号")
+    @ApiModelProperty(value = "手机号")
     private String mobile;
 
     /**
      * 邮箱
      */
     @TableField(value = "email")
-    @ApiModelProperty(value="邮箱")
+    @ApiModelProperty(value = "邮箱")
     private String email;
 
     /**
      * 状态 0-无效； 1-有效；
      */
     @TableField(value = "`status`")
-    @ApiModelProperty(value="状态 0-无效； 1-有效；")
+    @ApiModelProperty(value = "状态 0-无效； 1-有效；")
     private Byte status;
 
     /**
      * 创建人
      */
     @TableField(value = "create_by")
-    @ApiModelProperty(value="创建人")
+    @ApiModelProperty(value = "创建人")
     private Long createBy;
 
     /**
      * 修改人
      */
-    @TableField(value = "modify_by")
-    @ApiModelProperty(value="修改人")
+    @TableField(value = "modify_by", fill = FieldFill.UPDATE)
+    @ApiModelProperty(value = "修改人")
     private Long modifyBy;
 
     /**
      * 创建时间
      */
-    @TableField(value = "created")
-    @ApiModelProperty(value="创建时间")
+    @TableField(value = "created", fill = FieldFill.INSERT)
+    @ApiModelProperty(value = "创建时间")
     private Date created;
 
     /**
      * 修改时间
      */
-    @TableField(value = "last_update_time")
-    @ApiModelProperty(value="修改时间")
+    @TableField(value = "last_update_time", fill = FieldFill.UPDATE)
+    @ApiModelProperty(value = "修改时间")
     private Date lastUpdateTime;
+
+    /**
+     * 角色的IDS
+     */
+    @ApiModelProperty(value = "角色的IDS")
+    @TableField(exist = false)
+    private String role_strings;
 }
--- coin-exchange/coin-admin/admin-service/src/main/java/com/bjsxt/service/SysUserService.java
+++ coin-exchange/coin-admin/admin-service/src/main/java/com/bjsxt/service/SysUserService.java
@@ -1,5 +1,6 @@
 package com.bjsxt.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.bjsxt.domain.SysUser;
 import com.baomidou.mybatisplus.extension.service.IService;
     /**
@@ -10,4 +11,23 @@ import com.baomidou.mybatisplus.extension.service.IService;
 public interface SysUserService extends IService<SysUser>{
 
 
-}
+        /**
+         * 分页查询员工
+         * @param page
+         *  分页参数
+         * @param mobile
+         *  员工的手机号
+         * @param fullname
+         *  员工的全名称
+         * @return
+         */
+        Page<SysUser> findByPage(Page<SysUser> page, String mobile, String fullname);
+
+
+        /**
+         * 新增员工
+         * @param sysUser
+         * @return
+         */
+        boolean addUser(SysUser sysUser);
+    }
