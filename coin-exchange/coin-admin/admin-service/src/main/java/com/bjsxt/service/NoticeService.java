62bd1df42988ddcac2c8143410c2cdc6060db0b1
pengchenyu <1106998960@qq.com>
2021-06-14 14:32:18 +0800

    公告管理

--- coin-exchange/coin-admin/admin-service/src/main/java/com/bjsxt/service/NoticeService.java
+++ coin-exchange/coin-admin/admin-service/src/main/java/com/bjsxt/service/NoticeService.java
@@ -1,13 +1,33 @@
 package com.bjsxt.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.bjsxt.domain.Notice;
 import com.baomidou.mybatisplus.extension.service.IService;
-    /**
- * 
+
+/**
  * @author PengChenyu
  * @since 2021-06-10 16:59:16
  */
-public interface NoticeService extends IService<Notice>{
+public interface NoticeService extends IService<Notice> {
 
 
+    /**
+     * 条件查询公告
+     *
+     * @param page      分页参数
+     * @param title     公告的标签
+     * @param startTime 公告创建的开始时间
+     * @param endTime   公告创建的结束时间
+     * @param status    公告当前的状态
+     * @return
+     */
+    Page<Notice> findByPage(Page<Notice> page, String title, String startTime, String endTime, Integer status);
+
+    /**
+     * 查询公告
+     *
+     * @param page
+     * @return
+     */
+    Page<Notice> findNoticeForSimple(Page<Notice> page);
 }
